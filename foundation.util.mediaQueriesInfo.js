f2aada2adefd8d519fc7499b189d53d7c8ec7b2f
franklang <lang.frank79@gmail.com>
2017-07-07 09:30:04 +0200

    Create foundation.util.mediaQueriesInfo.js

---
+++ foundation.util.mediaQueriesInfo.js
@@ -0,0 +1,20 @@
+// Foundation 6 MediaQueries info function
+function zfMediaQueriesInfo() {
+  var currentSize = Foundation.MediaQuery.current;
+  console.log('Current: ' + currentSize);
+
+  $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
+    console.log('From: ' + oldSize + ' to: ' + newSize);
+
+    var medium2small = oldSize == 'medium' && newSize == 'small';
+    var medium2large = oldSize == 'medium' && newSize == 'large';
+    
+    if(medium2small){
+     console.log('from MEDIUM to SMALL');
+    }
+    if(medium2large){
+     console.log('from MEDIUM to LARGE');
+    }
+  });
+}
+zfMediaQueriesInfo();
