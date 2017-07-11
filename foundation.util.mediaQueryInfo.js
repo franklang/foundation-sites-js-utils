bafcc0ad82e7aaf7fe2a8a8a5fbf7f6b4ae1a740
franklang <lang.frank79@gmail.com>
2017-07-11 07:31:00 +0200

    Added a few more tricks

--- foundation.util.mediaQueryInfo.js
+++ foundation.util.mediaQueryInfo.js
@@ -1,8 +1,30 @@
 // Foundation 6 MediaQuery info function
 function zfMediaQueryInfo() {
-  var currentSize = Foundation.MediaQuery.current;
+  /*
+   * Get current size
+   */
+  var currentSize = Foundation.MediaQuery.current;  
   console.log('Current: ' + currentSize);
+  
+  /*
+   * Global variables
+   * Store breakpoints for either Mobile or Desktop views
+   */
+  var bpMobile = ['small', 'medium'];
+  var bpDesktop = ['large', 'xlarge', 'xxlarge'];  
 
+  /*
+   * If current breakpoint is Mobile, else is Desktop
+   */
+  if($.inArray(Foundation.MediaQuery.current, bpMobile) != -1){
+    alert('Mobile!');
+  } else {
+    alert('Desktop!');
+  }
+  
+  /*
+   * On breakpoint change - not to be mistaken with $(window).on('resize')
+   */
   $(window).on('changed.zf.mediaquery', function(event, newSize, oldSize) {
     console.log('From: ' + oldSize + ' to: ' + newSize);
 
